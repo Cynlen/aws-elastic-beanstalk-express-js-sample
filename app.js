c15a60462416f7617d02fd2b661afb344a0592cb
Cynlen <87099444+Cynlen@users.noreply.github.com>
2021-07-07 17:32:49 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Mic check, is this thing on!'));
+app.get('/', (req, res) => res.send('2nd Edit, hope this works!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
